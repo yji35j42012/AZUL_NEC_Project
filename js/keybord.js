756d5e208b54d003637897c1e1343b5c6614081a
yji35j42012 <35093568+yji35j42012@users.noreply.github.com>
2022-11-24 15:19:05 +0800

    檔案同步

--- js/keybord.js
+++ js/keybord.js
@@ -16,6 +16,9 @@ for (let i = 0; i < keybox_btn.length; i++) {
             key_submitHandler();
         } else if (item == "clear") {
             key_clearHandler();
+        } else if (item == "submit_company") {
+            // 111.11.17新增
+            key_submitCompanyHandler();
         } else {
             inputData.push(item);
         }
@@ -30,6 +33,14 @@ function insertInput() {
     key_input.value = inputStr;
 }
 function key_submitHandler() {
+    // 111.11.17新增
+    if (key_submit.getAttribute("data-submit")) {
+        key_submitCompanyHandler()
+        return;
+    }
+    // 111.11.17新增
+
+
     console.log("送出");
     // 如果證號在 class="input_box" 加上_error
     if (inputData.length < 9) {
@@ -50,3 +61,45 @@ function key_clearHandler() {
 }
 key_submit.addEventListener("click", key_submitHandler);
 key_clear.addEventListener("click", key_clearHandler);
+
+
+
+// 111.11.17新增
+function key_submitCompanyHandler() {
+    console.log("廠商送出");
+    if (key_submit.getAttribute("data-submit") == "company_submit") {
+        var checkString = new RegExp("[A-Za-z]+");
+        var strcount = 0;
+        for (let i = 0; i < inputData.length; i++) {
+            if (checkString.test(inputData[i])) {
+                strcount++;
+            }
+        }
+        // 第一個字母不是英文字
+        if (!checkString.test(inputData[0])) {
+            input_box.classList.add("_error");
+            console.log('第一個字母不是英文字');
+            return;
+        }
+        // 總數超過8字或低於8字
+        else if (inputData.length !== 8) {
+            input_box.classList.add("_error");
+            console.log('總數超過8字或低於8字');
+            return;
+        }
+        // 輸入項目並非1英文字母+7數字
+        else if (strcount > 1) {
+            input_box.classList.add("_error");
+            console.log('輸入項目並非1英文字母+7數字');
+            return;
+        }
+        // 以上皆正確，導向下一頁
+        else{
+            if (key_input.value == "A1234567") {
+                alert.classList.add("on");
+            }else {
+                from_face();
+            }
+        }
+    }
+}
